a35acd3a5dd8709cada71be6dc2514dd0e2e866f
aNKITT522 <ankittkumar52@gmail.com>
2023-08-04 17:11:08 +0530

    just renaming

--- model/dbConn.js
+++ model/dbConn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const db = process.env.DATABASE;
+const db = process.env.MONGODB_URI;
 mongoose.connect(db).then(()=>{
     console.log(`connected to db`);
 }).catch((err)=>{
-    console.log(`no connection`);
+    console.log(`no connection`,err);
 })
